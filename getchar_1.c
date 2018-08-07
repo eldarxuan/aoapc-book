ae70080f68a1b0aa7418ecfb71ba231203c767a7
Yuxuan Xu <852563495@qq.com>
2018-08-07 23:02:03 +0800

    problem of getchar

---
+++ getchar_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char ch1 , ch2;
+	ch1 = getchar();
+	printf("ch1=%c\n",ch1);
+	ch2 = getchar();
+	printf("ch2=%c\n",ch2);
+return 0;
+}
---
+++ getchar_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char ch1,ch2;
+        scanf("%c",&ch1);
+	printf("ch1=%c\n",ch1);
+	getchar();
+	scanf("%c",&ch2);
+	printf("ch2=%c\n",ch2);
+return 0;
+}
