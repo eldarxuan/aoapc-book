2d6ea4d5fd3daf3ea45e5cdfc1df139cb394ff7a
Yuxuan Xu <852563495@qq.com>
2018-08-26 19:29:19 +0800

    打折(discount)

--- ex_1_5.c
+++ ex_1_5.c
@@ -4,8 +4,10 @@ int main()
   int n;
   float m;
   scanf("%d",&n);
-  m=95*n;
-  if(m>300){m=m*0.85;}
-  printf("%.2f",m);
+  m = 95 * n;
+  if(m >= 300){
+    m = m * 0.85;
+  }
+    printf("%.2f",m);
   return 0;
 }
